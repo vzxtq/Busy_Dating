c815b6c9d3641440c843c6d78fbf8655a5398354
vzxtq <fixitex0206@gmail.com>
2025-01-12 00:52:49 +0100

    initial_commit9

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -87,7 +87,7 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
 }
 
