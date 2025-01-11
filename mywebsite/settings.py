6895fd3c5bd5f0ade44aae2ff8de11e5b8c57503
vzxtq <fixitex0206@gmail.com>
2025-01-12 00:57:32 +0100

    initial_commit99

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -125,15 +125,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+# If using whitenoise for serving static files (recommended for Heroku)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Ensure you have a location for static files, if required
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
