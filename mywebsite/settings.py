cb68b119bc889a24cab57e2ea146827ea721448f
vzxtq <fixitex0206@gmail.com>
2025-01-13 21:01:55 +0100

    dajngo_load_problems

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 
 import dj_database_url
+import django
+
+django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
