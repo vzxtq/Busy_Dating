0f99785c5339211fad935345bb2b0b2d30d7aadf
vzxtq <fixitex0206@gmail.com>
2025-01-14 17:03:06 +0100

    asgi_problems

--- mywebsite/asgi.py
+++ mywebsite/asgi.py
@@ -4,14 +4,18 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from base.routing import websocket_urlpatterns
 
+# Set the default settings module for Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
-application = get_asgi_application()
 
+# Create the Django ASGI application
+django_asgi_app = get_asgi_application()
+
+# Define the protocol type router
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,  # HTTP protocol
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            websocket_urlpatterns  # Define WebSocket routes
         )
     ),
 })
